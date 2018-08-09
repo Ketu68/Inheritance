78c40441f856919aaf5659c5b4259314455f4b53
Ketu68 <41552633+Ketu68@users.noreply.github.com>
2018-08-09 17:58:20 -0500

    Inheritance1

---
+++ Inheritance1/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance1
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            string FirstName = "Phillip", LastName = "Beaver";
+            Console.WriteLine("Full name is: "+FirstName+" "+LastName);
+        }
+        public void SayName(string fname, string lname)
+        {
+            Console.WriteLine("Full name is: " + fname + " " + lname);
+        }
+    }
+
+    public class Employee : Person
+    {
+        int ID;
+    }
+
+}
--- Inheritance1/Program.cs
+++ Inheritance1/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace Inheritance1
 {
-    class Program
+
+//1. Create a class called Person and give it two properties, each of data type string. One called FirstName, the other LastName.
+//
+//2. Give this class a void method called SayName() that takes no parameters and simply writes the person's full name to the console in the format of: "Name: [full name]".
+//
+//3. Create another class called Employee and have it inherit from the Person class. Give the Employee class a property called Id and have it be of data type int.
+//
+//4. Inside of the Main method, instantiate and initialize an Employee object with a first name of "Sample" and a last name of "Student".
+//
+//5. Call the superclass method SayName() on the Employee object.
+
+    static class Program
     {
         static void Main(string[] args)
         {
+            Person person = new Person();
+
+            person.SayName();
+
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+
+            person.SayName(employee.FirstName,employee.LastName);
+
+            Console.ReadLine();
         }
     }
 }
